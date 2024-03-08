5be090d6cdffb18b145a2da0553e783703fe3cf1
hs <tlsgustn93@naver.com>
2024-03-08 20:29:52 +0900

    t

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,4 +1,4 @@
 export const constant = {
-  // apiUrl: "http://52.78.127.145:8080/",
-  apiUrl: process.env.NEXT_PUBLIC_API,
+  apiUrl: "http://52.78.127.145:8081/",
+  // apiUrl: process.env.NEXT_PUBLIC_API,
 };
